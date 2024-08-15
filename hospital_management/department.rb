c036ab016cf27a56efbcc676656c6a0a9438f179
abiola <abiola.arobieke@gmail.com>
2024-08-15 10:07:51 -0700

    Add a method for adding staff

--- hospital_management/department.rb
+++ hospital_management/department.rb
@@ -1,10 +1,16 @@
 class Department
-  attr_reader :name, :location, :hospital
+  attr_reader :name, :location, :hospital, :staff
 
   def initialize(name, location, name_abbr)
     @name = name
     @location = location
     @name_abbr = name_abbr
+    @staff = []
+  end
+
+  def add_staff(worker)
+    staff.push(worker)
+    worker.department = self
   end
 
   def hospital=(hospital)
