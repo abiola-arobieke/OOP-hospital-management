339ec1ed8536e56e08b52a613b82ef79ac5e57e3
abiola <abiola.arobieke@gmail.com>
2024-08-15 08:07:10 -0700

    Add @date_joined instance attribute

--- hospital_management/staff.rb
+++ hospital_management/staff.rb
@@ -3,30 +3,29 @@ require_relative './person.rb'
 class Staff < Person
   attr_reader :name, :address, :gender
 
-  def initialize(name, address, gender)
+  def initialize(name, address, gender, date_joined)
+    @date_joined = date_joined
     super(name, address, gender)
   end
 end
 
 
 class Technical < Staff
-
-  def initialize(name, address, gender='m')
-    super(name, address, gender)
+  def initialize(name, address, gender='m', date_joined)
+    super(name, address, gender, date_joined)
   end
 end
 
 
 class Administrative < Staff
-  def initialize(name, address, gender='m')
-    super(name, address, gender)
+  def initialize(name, address, gender='m', date_joined)
+    super(name, address, gender, date_joined)
   end
 end
 
 
 class Operational < Staff
-  
-  def initialize(name, address, gender='m')
-    super(name, address, gender)
+  def initialize(name, address, gender='m', date_joined)
+    super(name, address, gender, date_joined)
   end
 end
