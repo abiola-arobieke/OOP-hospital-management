6f657801186b8e6741afc7dc3b2c7d39125cfd3f
abiola <abiola.arobieke@gmail.com>
2024-08-15 08:03:59 -0700

    Add getter and add_department methods

--- hospital_management/hospital.rb
+++ hospital_management/hospital.rb
@@ -1,9 +1,18 @@
 class Hospital
-  attr_reader :name, :address, :phone
+  attr_reader :name, :address, :phone, :medical
+  attr_accessor :department
   
   def initialize(name, address, phone)
     @name = name
-    @address = address,
+    @address = address
     @phone = phone
+    @department = []
+    @medical = []
   end
+
+  def add_department(department)
+    @department.push(department)
+    department.hospital = self
+  end
+
 end
