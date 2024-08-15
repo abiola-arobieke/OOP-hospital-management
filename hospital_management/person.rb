97ef9a3e6ce0517435f2a8e74e2c6aa9086ef848
abiola <abiola.arobieke@gmail.com>
2024-08-15 08:08:48 -0700

    Add instance attribute and getter

--- hospital_management/person.rb
+++ hospital_management/person.rb
@@ -1,9 +1,12 @@
 class Person
+    attr_reader :medical
+    
     def initialize(name, address, gender)
         @name = name
         @address = address
         @gender = gender
         @phone = ''
+        @medical = []
     end
 end
 
