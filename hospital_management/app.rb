68d3c4401750ac8cdc2a39b8d8d0090e2b693ec6
abiola <abiola.arobieke@gmail.com>
2024-08-15 12:34:03 -0700

    Test relationship in appointment

--- hospital_management/app.rb
+++ hospital_management/app.rb
@@ -4,9 +4,11 @@ require_relative './hospital'
 require_relative './department'
 require_relative './medical_record'
 require_relative './patient'
+require_relative './appointment'
 
 doc1 = Doctor.new('Adewale Johnson', 'Ayegun, Oleyo Rd')
 doc2 = Doctor.new('Mercy Abreu', 'Yanga, Edo Rd')
+doc3 = Doctor.new('Faith Olowo', 'Isake Eko, Gbagada')
 
 tech = Techincian.new('Alabi Brown', 'Orita Challengee')
 
@@ -21,11 +23,9 @@ records = Department.new('Medical Records', 'Prof Olorunda center', 'Records')
 
 n_and_d.add_staff(doc1)
 n_and_d.add_staff(doc2)
+n_and_d.add_staff(doc3)
 
 n_and_d.remove_staff(doc1)
-
-p n_and_d.staff.length
-
 
 saint_mary.add_department(n_and_d)
 saint_mary.add_department(records)
@@ -35,9 +35,16 @@ victor_medical = MedicalRecord.new(patient2, saint_mary)
 
 doc1_medical = MedicalRecord.new(doc1, saint_mary)
 
+app1 = Appointment.new(patient1, doc2, "2024-09-02")
 
-
-doc1.medical.each do |pers|
-  p pers.person.medical
+patient1.appointment.each do |appo|
+  p appo.operational_staff.name
 end
+
+
+
+
+# doc1.medical.each do |pers|
+#   p pers.person.medical
+# end
 
