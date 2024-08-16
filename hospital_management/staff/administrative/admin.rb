e35e7c406f49d84f6d03a4ae6906c907c8b23a4c
abiola <abiola.arobieke@gmail.com>
2024-08-16 13:18:19 -0700

    Move admin into administrative folder

---
+++ hospital_management/staff/administrative/admin.rb
@@ -0,0 +1,31 @@
+require_relative '../staff'
+
+class Administrative < Staff
+  def initialize(name, address, gender='m', date_joined)
+    super(name, address, gender, date_joined)
+  end
+
+  def delete_staff()
+
+  end
+
+  def edit_salary()
+
+  end
+
+  def calculate_salary()
+
+  end
+
+  def issue_notices()
+
+  end
+
+  def provide_report()
+
+  end
+
+  def update_database()
+    
+  end
+end
