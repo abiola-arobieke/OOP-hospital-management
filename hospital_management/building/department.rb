7cc0f6e22c4636e5866b0407e07b58db8fa8c7d4
abiola <abiola.arobieke@gmail.com>
2024-08-19 14:54:08 -0700

    Add a method for adding room to the department

--- hospital_management/building/department.rb
+++ hospital_management/building/department.rb
@@ -1,10 +1,11 @@
 class Department
-  attr_reader :name, :name_abbr, :building, :staff
+  attr_reader :name, :name_abbr, :building, :staff, :rooms
 
   def initialize(name, name_abbr)
     @name = name
     @name_abbr = name_abbr
     @staff = []
+    @rooms = []
   end
 
   def add_staff(employee)
@@ -17,6 +18,20 @@ class Department
       if worker == employee
         worker_index = staff.find_index(employee)
         staff.delete_at(worker_index)
+      end
+    end
+  end
+
+  def add_room(room)
+    @rooms.append(room)
+    room.department = self
+  end
+
+  def remove_room(room)
+    rooms.each do |department_room|
+      if department_room = room
+        department_room_index = rooms.find_index(room)
+        rooms.delete_at(department_room_index)
       end
     end
   end
