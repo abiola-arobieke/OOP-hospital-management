b5537456bda0b5e79063b4d759957657ab426c75
abiola <abiola.arobieke@gmail.com>
2024-08-19 14:51:21 -0700

    Add drugs.rb file to the project

---
+++ hospital_management/pharmacy/drugs.rb
@@ -0,0 +1,9 @@
+class Drug
+  def initialize(code, name, description, administration)
+    @code = code
+    @name = name
+    @description = description
+    @administration = administration
+  end
+end
+
