48628469f9be49a52af6bb4ae9b7becb5ea8388b
abiola <abiola.arobieke@gmail.com>
2024-08-20 15:23:06 -0700

    Update files

--- hospital_management/pharmacy/drugs.rb
+++
@@ -1,9 +0,0 @@
-class Drug
-  def initialize(code, name, description, administration)
-    @code = code
-    @name = name
-    @description = description
-    @administration = administration
-  end
-end
-
