2836350da4469d790cb1d2c1b1b6b56d9e354eb1
abiola <abiola.arobieke@gmail.com>
2024-08-20 15:11:18 -0700

    Add a method for administring drugs to patient

--- hospital_management/staff/medical/nurse.rb
+++ hospital_management/staff/medical/nurse.rb
@@ -1,8 +1,21 @@
 require_relative '../staff'
+require_relative '../../records/administered_drug'
+require_relative '../../records/patient_history'
 
 class Nurse < Operational
   def initialize(name, address, ward_assigned,  shift, gender='f')
     super(name, address, gender)
+  end
+
+  def administer_drug(patient, drug, dosage)
+    if drug.dosage > dosage
+      request = AdministeredDrug.new(dosage)
+      administer = drug.administer(request)
+      PatientHistory.new("2024-08-09", "Hospitalized", patient, request)
+    else
+      puts "Insufficient dosage"
+    end
+    
   end
 
   def give_medicine()
