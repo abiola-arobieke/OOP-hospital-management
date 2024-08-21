9e8e3a89b758781ee2cfdc2a84e373a4f1aeee5e
abiola <abiola.arobieke@gmail.com>
2024-08-21 13:41:50 -0700

    Modify prescription.rb file

--- hospital_management/records/prescription.rb
+++ hospital_management/records/prescription.rb
@@ -1,15 +1,17 @@
+require 'date'
+
 class Prescription
   attr_reader :patient, :operational_staff, :date, :medicine
 
-  def initialize(patient, operational_staff, date, *medicine)
+  def initialize(patient, operational_staff, *medicine)
     @patient = patient
     patient.prescriptions << self
 
     @operational_staff = operational_staff
     operational_staff.prescriptions << self
 
-    @date = date
+    @date = DateTime.now.strftime("%Y/%m/%d")
     @medicine = medicine
   end
-
+  
 end
