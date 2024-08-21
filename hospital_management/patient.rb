73509552572fa123f81c092bfc64df003b2bd46b
abiola <abiola.arobieke@gmail.com>
2024-08-21 13:46:05 -0700

    Modify history method

--- hospital_management/patient.rb
+++ hospital_management/patient.rb
@@ -2,22 +2,38 @@ require_relative './person.rb'
 require_relative './records/appointment'
 
 class Patient < Person
-  attr_reader :appointments, :prescriptions, :history, :ward
+  attr_reader :appointments, :prescriptions, :ward, :diagnosis, :visit, :administered, :health_record, :status
 
-  def initialize(name, address, gender="m")
+  def initialize(name, address, gender="m", status="away")
     super(name, address, gender)
     @appointments = []
     @prescriptions = []
-    @history = []
+    @health_record = []
+    @diagnosis = []
+    @visit = []
+    @administered = []
+    @status = status
   end
 
   def details()
     return self
   end
 
+  def status=(status)
+    @status = status
+  end
+
+  def history(status, doc_or_nurse, drug_giving, date)
+    PatientHistory.new(status, self, doc_or_nurse, drug_giving, date)
+  end
+
   def ward=(ward)
     @ward = ward
     ward.patients.push(ward) unless ward.patients.include?(ward)
+  end
+
+  def book_appointment(doc_or_nurse, date)
+    Appointment.new(self, doc_or_nurse, date)
   end
 
   def add_to_history(health_note)
@@ -31,9 +47,5 @@ class Patient < Person
 
   def receieve_report()
 
-  end
-
-  def book_appointment(doc_or_nurse, date)
-    Appointment.new(self, doc_or_nurse, date)
   end
 end
