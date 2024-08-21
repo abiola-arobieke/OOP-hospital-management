e49a7fdc6776603dfdcac2fdffa8bac28cbf3bc0
abiola <abiola.arobieke@gmail.com>
2024-08-21 13:46:41 -0700

    Update drug.rb file

--- hospital_management/pharmacy/drug.rb
+++ hospital_management/pharmacy/drug.rb
@@ -14,16 +14,4 @@ class Drug
     @pharmarcist = pharmarcist
     pharmarcist.drugs.push(self) unless pharmarcist.drugs.include?(self)
   end
-
-  def administer(request)
-    if request.dosage <= @dosage
-      @dosage -= request.dosage
-      @administered.push(request)
-      request.drug = self
-      true
-    else
-      puts 'Request denied! Insufficient dosage in the store!!'
-      false
-    end
-  end
 end
