96cc9acf11536105a18685f6112a395b64b10c12
abiola <abiola.arobieke@gmail.com>
2024-08-23 15:51:50 -0700

    Add a method for scheduling patient

--- hospital_management/staff/administrative/receptionist.rb
+++ hospital_management/staff/administrative/receptionist.rb
@@ -1,5 +1,17 @@
 require_relative './front_desk'
+require_relative '../../patient.rb'
+require_relative '../../records/appointment'
 
 class Receptionist < FrontDesk
-  
+  def initialize(name, address, gender='m', date_joined)
+    super(name, address, gender='m', date_joined)
+  end
+
+  def register_patient(name, address, gender="m", status="away")
+    Patient.new(name, address, gender, status)
+  end
+
+  def schedule_appointment(patient, doc_or_nurse, date)
+    Appointment.new(patient, doc_or_nurse, date)
+  end
 end
